1805a1ff8cc26c6d875d665aff8043425b38644d
Luv sahu <lksahu404@gmail.com>
2022-07-04 10:31:11 +0530

    [Luv SAHU] This code for print Pattern

---
+++ Pattrn/Pattern.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraticeProblems
+{
+    internal class Pattern
+    {
+        public void Pttrn()
+        {
+            int i, j, k;
+            Console.WriteLine("Enter the number");
+            k = int.Parse(Console.ReadLine());
+            
+
+            for (i = 1; i <= k; i++)
+            {
+                for (j = k; j>i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                for (j = i - 1; j >= 1; j--)
+                {
+                    Console.Write(j);
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ Pattrn/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace PraticeProblems
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+
+            Console.WriteLine("Choose a number to go to the respective problem \n");
+            Console.WriteLine("1 for Pattern");
+            Console.WriteLine("2 for Swapping without third variable");
+            Console.WriteLine("3 for Multiply number");
+            Console.WriteLine("4 for Find Max Min and Avg of num");
+            Console.WriteLine("5 for Check character is Vowel and consonant");
+
+
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Pattern pt = new Pattern();
+                    pt.Pttrn();
+                    break;
+
+                case 2:
+                    SwappingWithoutUsingThirdvariable swap = new SwappingWithoutUsingThirdvariable();
+                    swap.Swap();
+                    break;
+
+                case 3:
+                    Multiply multi = new Multiply();
+                    multi.Mul();
+                    break;
+
+                case 4:
+                    FindNumber findNumber = new FindNumber();
+                    findNumber.Number();
+                    break;
+
+                case 5:
+                    CheckVowelsConsonant vowelsConsonant = new CheckVowelsConsonant();
+                    vowelsConsonant.VwlConsonant();
+                    break;
+            }
+        }
+
+    }
+}
